5819196a085da9b4f21205e2fea87b5888e9ab0a
bitarx <imanishi@bitarx.jp>
2017-01-07 22:49:03 +0900

    defaultを0に変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170103014335) do
+ActiveRecord::Schema.define(version: 20170107132553) do
 
   create_table "rentals", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "kind"
     t.string   "name"
     t.text     "description", limit: 65535
-    t.integer  "delete_flg"
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.integer  "delete_flg",                default: 0
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
   end
 
 end
